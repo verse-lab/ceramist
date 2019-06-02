a9b81333683de9c366e19492aaa91968fc7d011d
Kiran Gopinathan <zcabkgo@ucl.ac.uk>
2019-06-02 22:14:21 +0100

    implemented bloomvector add function (need to change proof to function definition at some point)

--- Structures/BloomFilter.v
+++ Structures/BloomFilter.v
@@ -1,5 +1,5 @@
 From mathcomp.ssreflect
-     Require Import ssreflect ssrbool ssrnat eqtype fintype choice ssrfun seq path.
+     Require Import ssreflect ssrbool ssrnat eqtype fintype choice ssrfun seq path bigop finfun .
 
 From mathcomp.ssreflect
      Require Import tuple.
@@ -27,14 +27,21 @@ Section BloomFilter.
   Variable k: nat.
   (*
     n - maximum number of hashes supported
-  *)
+   *)
   Variable n: nat.
+  Variable Hkgt0: k >0.
+
+
+  Lemma Hpredkvld: k.-1 < k.
+    Proof.
+        by  apply InvMisc.ltnn_subS.
+    Qed.
 
 
   Record BloomFilter := mkBloomFilter {
-                                bloomfilter_hashes: k.-tuple (HashState n);
-                                bloomfilter_state: BitVector
-                              }.
+                            bloomfilter_hashes: k.-tuple (HashState n);
+                            bloomfilter_state: BitVector
+                          }.
 
   Definition BloomFilter_prod (bf: BloomFilter) := (bloomfilter_hashes bf, bloomfilter_state bf).
   Definition prod_BloomFilter  pair := let: (hashes, state) := pair in mkBloomFilter  hashes state.
@@ -65,28 +72,67 @@ Section BloomFilter.
   Canonical bloomfilter_finType :=
     Eval hnf in FinType BloomFilter  bloomfilter_finMixin.
 
-  (* The first approximation: a number of axioms *)
-  About set_tnth.
-
 
   Definition bloomfilter_set_bit (value: 'I_(Hash_size.+1)) bf : BloomFilter :=
     mkBloomFilter
       (bloomfilter_hashes bf)
-    (set_tnth (bloomfilter_state bf) true value).
+      (set_tnth (bloomfilter_state bf) true value).
+
+  Definition bloomfilter_get_bit (value: 'I_(Hash_size.+1)) bf : bool :=
+      (tnth (bloomfilter_state bf) value).
+
+  Definition bloomfilter_calculate_hash (index: 'I_k) (input: B) (bf: BloomFilter) : Comp [finType of (BloomFilter * 'I_(Hash_size.+1))] :=
+    let: hash_state := tnth (bloomfilter_hashes bf) index in
+    hash_out <-$ (@hash _ input hash_state);
+      let: (new_hash_state, hash_value) := hash_out in
+      ret (mkBloomFilter (set_tnth (bloomfilter_hashes bf) new_hash_state index) (bloomfilter_state bf), hash_value).
 
 
-               Definition bloomfilter_add (value: B) (bf: BloomFilter) : Comp [finType of BloomFilter].
-               Admitted.
+  Definition bloomfilter_update_state (index: 'I_k) (hash_result: Comp [finType of (BloomFilter * 'I_(Hash_size.+1))]) : Comp [finType of BloomFilter] :=
+    result <-$ hash_result;
+      let: (new_bf, hash_index) := result in
+      ret (bloomfilter_set_bit hash_index new_bf).
+                                  
 
 
-               Fixpoint bloomfilter_add_internal (value: B) (bf: BloomFilter) : BloomFilter.
-               Proof.
-                 move: bf => [[xs H_xs] bvec].
-                 Admitted.
+
+  Definition comp_fst (A B : finType) (cmp: Comp [finType of (A * B)]) :=
+    result <-$ cmp;
+      let: (a,b) := result in
+      ret a.
+
+  Definition comp_snd (A B : finType) (cmp: Comp [finType of (A * B)]) :=
+    result <-$ cmp;
+      let: (a,b) := result in
+      ret b.
 
 
-                 Definition bloomfilter_query (value: B) (bf: BloomFilter ) : bool.
-                 Admitted.
+  Fixpoint bloomfilter_add_internal (value: B) (bf: BloomFilter) (pos: nat) (Hpos: pos < k) : Comp [finType of BloomFilter].
+    case Heqpos': pos => [|pos'].
+    (* Case 1: pos is 0 *)
+        (* then update bitvector and return *)
+        move: (bloomfilter_calculate_hash (Ordinal Hpos) value bf) => /(bloomfilter_update_state (Ordinal Hpos)) updated_state.
+        exact updated_state.
+
+    (* Case 2: pos is pos.+1 *)
+        move: (Hpos); rewrite Heqpos' -(addn1 pos') => /InvMisc.addr_ltn Hpos'.
+        (* then update the bitvector*)
+        move: (bloomfilter_calculate_hash (Ordinal Hpos) value bf) => /(bloomfilter_update_state (Ordinal Hpos)) updated_state.
+        (* and recurse on a smaller argument *)
+        exact (@Bind _ _ updated_state
+                      (fun new_bf => 
+                                       bloomfilter_add_internal value new_bf pos' Hpos'
+                      )
+               ).
+        Qed.
+
+
+  Definition bloomfilter_add (value: B) (bf: BloomFilter) : Comp [finType of BloomFilter] := bloomfilter_add_internal value bf Hpredkvld.
+
+
+
+  Definition bloomfilter_query (value: B) (bf: BloomFilter ) : bool.
+  Admitted.
 
 
 End BloomFilter.
